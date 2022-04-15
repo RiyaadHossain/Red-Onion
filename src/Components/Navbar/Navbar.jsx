a8365ad1388307833e7b6e77b672e38c06a0fd1b
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-15 08:07:39 +0600

    Navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { BsCart3 } from 'react-icons/bs';
+import Logo from '../../Images/logo2.png'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='flex items-center justify-between h-20 container mx-auto'>
             <div>
-                <h1>Logo</h1>
+                <img className='w-32' src={Logo} alt="" />
             </div>
-            <nav>
-                <Link to={'/cart'}><BsCart3/></Link>
-                <Link to={'/signin'}>Log In</Link>
-                <Link to={'/signup'}>Sign Up</Link>
+            <nav className='flex items-center'>
+                <Link className='ml-8 text-xl font-bold' to={'/cart'}><BsCart3/></Link>
+                <Link className='ml-8 text-lg font-semibold' to={'/signin'}>Log In</Link>
+                <Link className='ml-8 text-lg font-semibold bg-red-600 py-1 rounded-full text-white px-5' to={'/signup'}>Sign Up</Link>
             </nav>
         </div>
     );
