2928d1621bd06fc47032ab693335e5c78e485efd
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-15 15:59:49 +0600

    log in added

--- src/Components/Auth/SignIn/SignIn.jsx
+++ src/Components/Auth/SignIn/SignIn.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import auth from "../../../Firebase/Firebase.init";
 import Logo from "../../../Images/logo2.png";
+import { useAuthState, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const SignIn = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user1, error1] = useAuthState(auth);
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+  const onEmailSubmit = e => {
+    e?.preventDefault()
+    signInWithEmailAndPassword(email, password)
+    
+    // It's not working
+    if (user1) {
+      console.log(user);
+      navigate('/')
+      
+    } else {
+      console.log(error);
+    }
+  }
+  
   return (
     <div className="SignIn-container h-[61.7vh]">
       <div className="flex justify-center items-center py-16">
         <div>
           <img className="w-40 mx-auto" src={Logo} alt="" />
-          <form>
-            <input className="block mt-5 py-3 w-96 pl-3 text-lg outline-none" type="text" placeholder="Email" />
-            <input className="block mt-5 py-3 w-96 pl-3 text-lg outline-none" type="password" placeholder="Password" />
-            <input className="block my-5 w-96 bg-red-600 text-white py-3" type="submit" value="Sign In" />
-          </form>
+          <div >
+            <input onBlur={e => setEmail(e.target.value)} className="block mt-5 py-3 w-96 pl-3 text-lg outline-none" type="text" placeholder="Email" />
+            <input onBlur={e => setPassword(e.target.value)} className="block mt-5 py-3 w-96 pl-3 text-lg outline-none" type="password" placeholder="Password" />
+            <input onClick={() =>onEmailSubmit()}  className="block my-5 w-96 bg-red-600 text-white py-3" type="submit" value="Sign In" />
+          </div>
           <button onClick={() => navigate('/signup')} className="text-red-600 mx-auto block text-lg font-semibold">New to Red Onion</button>
         </div>
       </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { BsCart3 } from 'react-icons/bs';
-import Logo from '../../Images/logo2.png'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
+import Logo from "../../Images/logo2.png";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../Firebase/Firebase.init";
+import { signOut } from "firebase/auth";
+import toast from "react-hot-toast";
 
 const Navbar = () => {
-    const navigate = useNavigate()
-    return (
-        <div className='flex items-center justify-between h-20 container mx-auto'>
-            <div onClick={() => navigate('/')}>
-                <img className='w-32' src={Logo} alt="" />
-            </div>
-            <nav className='flex items-center'>
-                <Link className='ml-8 text-xl font-bold' to={'/cart'}><BsCart3/></Link>
-                <Link className='ml-8 text-lg font-semibold' to={'/signin'}>Log In</Link>
-                <Link className='ml-8 text-lg font-semibold bg-red-600 py-1 rounded-full text-white px-5' to={'/signup'}>Sign Up</Link>
-            </nav>
-        </div>
-    );
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("Logged Out");
+      })
+      .catch((error) => {
+        toast.error("A problem occured");
+      });
+  };
+  return (
+    <div className="flex items-center justify-between h-20 container mx-auto">
+      <div onClick={() => navigate("/")}>
+        <img className="w-32" src={Logo} alt="" />
+      </div>
+      <nav className="flex items-center">
+        <Link className="ml-8 text-xl font-bold" to={"/cart"}>
+          <BsCart3 />
+        </Link>
+        {user ? (
+          <button onClick={logOut} className="ml-8 text-lg font-semibold">
+            Log Out
+          </button>
+        ) : (
+          <Link className="ml-8 text-lg font-semibold" to={"/signin"}>
+            Log In
+          </Link>
+        )}
+        <Link
+          className="ml-8 text-lg font-semibold bg-red-600 py-1 rounded-full text-white px-5"
+          to={"/signup"}
+        >
+          Sign Up
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
